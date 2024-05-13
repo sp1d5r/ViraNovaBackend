cdcb5bcbcf7fe604620fa512adc6f88e04826c92
Elijah <aa1719@imperial.ac.uk>
2024-05-13 21:30:43 +0700

    Backend: Added audio file splitting mechanism.

--- routes/temporal_segmentation.py
+++ routes/temporal_segmentation.py
@@ -1,9 +1,13 @@
+import ast
+
 from flask import Blueprint, jsonify
 from services.firebase import FirebaseService
 from services.langchain_chains.crop_segment import requires_cropping_chain, delete_operation_chain
 from services.langchain_chains.idea_generator_chain import idea_generator_chain
-from services.verify_video_document import parse_and_verify_short
+from services.verify_video_document import parse_and_verify_short, parse_and_verify_video, parse_and_verify_segment
 from datetime import datetime
+from pydub import AudioSegment
+from io import BytesIO
 
 # Route Functions
 
@@ -155,4 +159,94 @@ def perform_temporal_segmentation(short_id):
         return error_message, 404
 
 
+
+def handle_operations_from_logs(logs, words):
+    delete_operations = [i for i in logs if i['type'] == "delete"]
+    delete_positions = [{'start': i['start_index'], 'end': i['end_index']} for i in delete_operations]
+
+    for word in words:
+        word['position'] = "keep"
+
+    for operation in delete_positions:
+        for i in range(operation['start'], operation['end'] + 1):
+            words[i]['position'] = 'delete'
+
+
+    # Apply operations
+    output_words = []
+    for word in words:
+        if word['position'] == 'delete':
+            continue
+        if word['position'] == 'keep':
+            output_words.append(word)
+
+    return output_words
+
+
+
+@temporal_segmentation.route("/generate-test-audio/<short_id>")
+def generate_test_audio(short_id):
+    firebase_service = FirebaseService()
+    short_document = firebase_service.get_document("shorts", short_id)
+    is_valid_document, error_message = parse_and_verify_short(short_document)
+    if is_valid_document:
+        print("Collected Document")
+        logs = short_document['logs']
+
+        if "video_id" in short_document.keys():
+            video_id = short_document['video_id']
+        else:
+            return "Failed - no video id in the short...", 300
+
+        video_document = firebase_service.get_document('videos', video_id)
+
+        is_valid_document, error_message = parse_and_verify_video(video_document)
+
+        if not is_valid_document:
+            return error_message, 404
+        else:
+            print("Collected video document")
+
+        audio_file = video_document['audio_path']
+
+        segment_document = firebase_service.get_document('topical_segments', short_document['segment_id'])
+
+        is_valid_document, error_message = parse_and_verify_segment(segment_document)
+
+        if not is_valid_document:
+            return error_message, 404
+        else:
+            print("Collected semgnet document")
+
+        segment_document_words = ast.literal_eval(segment_document['words'])
+        print("Read Segment Words")
+        words_to_handle = handle_operations_from_logs(logs, segment_document_words)
+        print("Download Audio File to Memory")
+        audio_stream = firebase_service.download_file_to_memory(audio_file)
+        print("Create temporary audio file")
+        audio_data = AudioSegment.from_file_using_temporary_files(audio_stream)
+
+        combined_audio = AudioSegment.silent(duration=0)
+        print("Perform operations}")
+        for word in words_to_handle:
+            start_time = int(word['start_time'] * 1000)
+            end_time = int(word['end_time'] * 1000)
+            segment = audio_data[start_time:end_time]
+            combined_audio += segment
+
+        print("Loading the bytes stream")
+        byte_stream = BytesIO()
+        combined_audio.export(byte_stream,
+                             format='mp4')  # Use 'mp4' as the format; adjust as necessary for your audio type
+
+        new_blob_location = 'temp-audio/' + "".join(audio_file.split("/")[1:])
+
+        byte_stream.seek(0)
+        file_bytes = byte_stream.read()
+        firebase_service.upload_audio_file_from_memory(new_blob_location, file_bytes)
+        print("Uploaded Result")
+        firebase_service.update_document('shorts', short_id, {'temp_audio_file': new_blob_location})
+        return 200, new_blob_location
+    else:
+        return 404, error_message
 
